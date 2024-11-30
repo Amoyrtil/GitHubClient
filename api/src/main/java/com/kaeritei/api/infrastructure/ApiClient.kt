e5d6f8ea15d70bf4f23f2481a6106192e4e55d14
Ryoma Shibata <r.shibata@cluster.mu>
2024-11-30 10:26:33 +0900

    add api client implementation

--- api/src/main/java/com/kaeritei/api/infrastructure/ApiClient.kt
+++ api/src/main/java/com/kaeritei/api/infrastructure/ApiClient.kt
@@ -1,8 +1,40 @@
 package com.kaeritei.api.infrastructure
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class ApiClient(
-    private val baseUrl: String,
-    private val authenticationInterceptor: AuthenticationInterceptor,
+    baseUrl: String,
+    authenticationInterceptor: AuthenticationInterceptor,
 ) {
-    // TODO: add api client implementation
+    private val loggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    private val okHttpClient =
+        OkHttpClient().newBuilder()
+            .addInterceptor(authenticationInterceptor)
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+
+    private val moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    private val retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    fun <S> createService(serviceClass: Class<S>): S {
+        return retrofit.create(serviceClass)
+    }
 }
